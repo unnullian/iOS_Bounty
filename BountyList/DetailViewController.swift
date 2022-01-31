6a308d9a58d1a50359d12dc63afc9480cc3beabe
MinwooKwak <unknown90420@naver.com>
2022-01-31 20:43:22 +0900

    - 애니메이션 기능 추가

--- BountyList/DetailViewController.swift
+++ BountyList/DetailViewController.swift
@@ -11,12 +11,38 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var lblBounty: UILabel!
     @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblNameCenterX: NSLayoutConstraint!
+    @IBOutlet weak var lblBountyCenterX: NSLayoutConstraint!
     
     let viewModel = DetailViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        prepareAnimation()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showAnimation()
+    }
+    
+    private func prepareAnimation() {
+        lblNameCenterX.constant = view.bounds.width
+        lblBountyCenterX.constant = view.bounds.width
+    }
+    
+    private func showAnimation() {
+        lblNameCenterX.constant = 0
+        lblBountyCenterX.constant = 0
+        
+//        UIView.animate(withDuration: 0.1) {
+//            self.view.layoutIfNeeded()
+//        }
+        
+        UIView.animate(withDuration: 0.3, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.2, options: .allowUserInteraction, animations: self.view.layoutIfNeeded, completion: nil)
+        
+        UIView.transition(with: imgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
     }
     
     @IBAction func showDetail(_ sender: Any) {
